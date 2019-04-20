1a1bbcae9f9318a3dc36d91f72a2ec8247391642
Maryna <maritkacheva@hotmail.com>
2019-04-20 23:49:19 +0200

    js_hw_01

---
+++ JSModule01/index.js
@@ -0,0 +1,72 @@
+'use strict';
+
+//task 01
+
+let message;
+const ADMIN_PASSWORD = 'm4ng0h4ckz';
+
+const userInput = prompt('Пожалуйста, введите пароль!');
+
+if(userInput === null) {
+  console.log(message = 'Отменено пользователем!');
+}else if (userInput === ADMIN_PASSWORD){
+  console.log(message = 'Добро пожаловать!');
+}else {
+  console.log(message = 'Доступ запрещен, неверный пароль!');
+}
+alert(message);
+
+//task 02
+
+const credits = 23580;
+const pricePerDroid = 3000;
+
+let totalPrice;
+
+let userInput = prompt('Укажите количество дроидов Вы желаете приобрести');
+
+if (userInput === null){
+  console.log('Отменено пользователем!');
+}else if (Number(userInput) >= 1){
+  console.log(totalPrice = pricePerDroid * userInput)
+    if (Number(totalPrice > credits)) {
+      console.log('Недостаточно средств на счету!');
+    } else if (Number(totalPrice <= credits)) {
+      totalPrice = credits - totalPrice;
+      console.log(`Вы купили ${userInput} дроидов, на счету осталось ${totalPrice} кредитов.`);
+    } 
+}
+
+//task 03
+
+let userInput = prompt('Введите страну доставки!');
+
+let priceChina =  100;
+let priceNorthAmerica = 250;
+let priceAustralia = 170;
+let priceIndia = 80;
+let priceJamaica =  120;
+
+if(userInput === null){
+  console.log('Отменено пользователем');
+}else {
+  switch(userInput = userInput.toLowerCase()){
+    case 'китай':
+    console.log(`'Доставка в ${userInput} будет стоить ${priceChina} кредитов'`);
+    break;
+    case 'южная америка':
+    console.log(`'Доставка в ${userInput} будет стоить ${priceNorthAmerica} кредитов'`);
+    break;
+    case 'австралия':
+    console.log(`'Доставка в ${userInput} будет стоить ${priceAustralia} кредитов. '`);
+    break;
+    case 'индия':
+    console.log(`'Доставка в ${userInput} будет стоить ${priceIndia} кредитов.'`);
+    break;
+    case 'ямайка':
+    console.log(`'Доставка в ${userInput} будет стоить ${priceJamaica} кредитов. '`);
+    break;
+    default:
+    console.log('В Вашей стране доставка не доступна');
+  }
+}
