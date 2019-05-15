c5c2afd8529a6be43255fb2abe04cdddff762c21
Maryna <maritkacheva@hotmail.com>
2019-05-15 23:40:51 +0300

    js_hw_03

--- JSModule03/index.js
+++ JSModule03/index.js
@@ -1,34 +1,37 @@
 'use strict'
 
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
-let login = prompt('Enter your login!');
 
 const isLoginValid = function(login) {
-
-  if(login.length < 4 || login.length > 16){
-    alert('Ошибка! Логин должен быть от 4 до 16 символов'); 
-    return false;
+  if(login.length >= 4 && login.length <= 16){
+    return true;
   }
- return true; 
+  return false;
 }
 
 const isLoginUnique = function(logins, login) {
-  if((logins.includes(login))){
-    alert('Такой логин уже используется!');
+  if(logins.includes(login)){
     return false;
   }
   return true;
-};
+}
 
 const addLogin = function(logins, login) {
-  if(isLoginValid(login)){  
-     if(isLoginUnique(logins, login)){
-      logins.push(login);
-      alert('Логин успешно добавлен!');
-    }
-  } 
-};
+  if(isLoginValid(login) === false){
+    return 'Ошибка! Логин должен быть от 4 до 16 символов';
+  }
 
-addLogin(logins, login);
+  if(isLoginUnique(logins, login) === false){
+    return 'Такой логин уже используется!';
+    } else{
+      logins.push(login);
+      return ('Логин успешно добавлен!');
+    }
+} 
+
+console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
+console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
+console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
 
 
